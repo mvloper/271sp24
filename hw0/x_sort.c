f6cedadcb6dde9dfe4c318d0835c91a54ce87d36
Dash <121083052+thebiene@users.noreply.github.com>
2024-02-07 15:05:05 -0800

    add print_arr function

--- hw0/x_sort.c
+++ hw0/x_sort.c
@@ -5,6 +5,18 @@
 // Input: An array of integers and its length
 // Output: Nothing
 // Side Effect: The input array is sorted from least to greatest
+void print_arr(int *arr, int len){
+	printf("printing arr at location %p of size %d:\n [ ", arr, len);
+	int i;
+	for (i = 0; i < len; i++){
+		printf("%d", arr[i]);
+		if (i < len - 1){
+			printf(", ");
+		}
+	}
+	printf(" ]\n");
+}
+
 void sort(int *arr, int len) // bubble sort :|
 {
 	int i, j;
@@ -48,7 +60,9 @@ int is_sorted(int *arr, int len)
 int main()
 {
 	int test_arr[10] = {8,6,4,2,0,1,3,5,7,9};
+	print_arr(test_arr, 10);
 	sort(test_arr, 10);
+	print_arr(test_arr, 10);
 	if (is_sorted(test_arr, 10))
 	{
 		printf("Good job!\n");
